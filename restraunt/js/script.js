f215f124b4565dd73e04211c92e39bc3364503a5
Dev <devsharma23101999@gail.com>
2020-04-06 03:11:31 +0530

    spa

--- restraunt/js/script.js
+++ restraunt/js/script.js
@@ -19,8 +19,9 @@ $(function() {
     };
 
     var show_loading = function(selector){
-        var html = "<div class='text-center' style='margin:15px'><img src='../img/ajax-loader.gif'></div>";
-        insert_html(selector,html);
+        var html = "<div class='text-center' style='margin:15px;'><img src='/img/ajax-loader.gif'></div>";
+        insert_html(selector, html);
+        console.log("laoding");
     };
 
     document.addEventListener("DOMContentLoaded", function (event) {
