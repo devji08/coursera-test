c6eef4dcbba12bd8d2cb4160da1e9c1e1f0b4fcb
Dev <devsharma23101999@gail.com>
2020-04-17 01:01:11 +0530

    redux

--- react/confusion/src/components/DishDetailComponent.js
+++ react/confusion/src/components/DishDetailComponent.js
@@ -1,61 +1,75 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class DishDetail extends Component {
-    constructor (props){
-        super(props);
-    }
-    renderDish(dish){
-        return(
-            <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
+
+
+function RenderDish({dish}){
+    return(
+        <Card>
+            <CardImg top src={dish.image} alt={dish.name} />
+            <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+function RenderComments({comments}){
+    if(comments==null){
+        return (
+            <div></div>
         );
     }
-    renderComments(comments){
-        const listofComment = comments.map((commentobj) => {
+    else{
+        const listofComment = comments.map((comment) => {
             return(
-                <li>
-                    <p>{commentobj.comment}</p>
-                    <p>{commentobj.author}{commentobj.date}</p>
+                <li key={comment.id}>
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                 </li>
             );
         });
-        if(comments==null)
-            return (
-                <div></div>
-            );
-        else
-            return(
-                <div>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {listofComment}
-                    </ul>
-                </div>
-            );
+        return(
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {listofComment}
+                </ul>
+            </div>
+        );
     }
-    render() {
-        if(this.props.dish==null)
-            return(
-                <div></div>
-            );
-        else 
-            return(
+}
+
+const DishDetail = (props) => {
+    if(props.dish==null){
+        return(
+            <div></div>
+        );
+    }
+    else{ 
+        return (
+            <div className="container">
                 <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
                     </div>
-                    <div className="col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
-                
-            );
+            </div>
+        );
     }
 }
 
